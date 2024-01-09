d8eb0a5c54f3d4228ebb07f7f88bd9bf2ffe53e2
Blastose <blastoisejay@gmail.com>
2024-01-08 20:17:24 -0800

    Add subreddit banner

--- src/routes/r/[subreddit]/+layout.svelte
+++ src/routes/r/[subreddit]/+layout.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
+	import { page } from '$app/stores';
+	import Banner from '$lib/components/subreddit/Banner.svelte';
+
 	export let data;
+
+	// We show the hot, new, rising, etc. buttons if the page isn't a comment thread
+	$: showSubredditOptions = !/^https?:\/\/[A-z0-9:.-]+\/r\/[A-z_0-9]+\/comments\/.*?$/.test(
+		$page.url.toString()
+	);
 </script>
 
 <p>
@@ -9,6 +17,7 @@
 	<a href="/">Home</a>
 </p>
 
+<Banner about={data.about} />
 <div>
 	{data.about.display_name_prefixed}
 	{#await data.sidebarPromise then sidebar}
