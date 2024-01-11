23546cd4da1a0927c073dafde3f5eda7d5d29552
Blastose <blastoisejay@gmail.com>
2024-01-11 11:59:31 -0800

    Fix bg image

--- src/lib/components/subreddit/embed/RedditImage.svelte
+++ src/lib/components/subreddit/embed/RedditImage.svelte
@@ -4,19 +4,20 @@
 
 	export let submission: SubmissionData;
 
-	$: defaultImage = submission.preview.images.at(0)?.resolutions.at(0);
+	$: defaultImage = submission.preview.images.at(0)?.source;
 	$: defaultImageUrl = defaultImage?.url ?? '';
 	$: srcsetAndSizes = getSrcsetAndSizes(submission);
+	$: bgImage = submission.preview.images.at(0)?.resolutions.at(0);
+	$: bgImageUrl = bgImage?.url ?? '';
 	$: aspectRatio = (defaultImage?.width ?? 0) / (defaultImage?.height ?? 1);
 
 	$: spoiler = submission.spoiler;
 	$: nsfw = submission.over_18;
 </script>
 
-{defaultImage?.width}
 <div
 	class="background-stuff relative z-0 max-h-[512px] overflow-hidden rounded-2xl border border-[#303030]"
-	style="background-image: linear-gradient(rgba(27, 27, 27, 0.8), rgba(27, 27, 27, 0.8)), url({defaultImageUrl});"
+	style="background-image: linear-gradient(rgba(27, 27, 27, 0.8), rgba(27, 27, 27, 0.8)), url({bgImageUrl});"
 >
 	<div class="background-filter"></div>
 	<img
