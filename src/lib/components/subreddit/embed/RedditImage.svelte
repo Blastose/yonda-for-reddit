a156ef8dc6d0c1ca225df11f1af91d61273c1287
Blastose <blastoisejay@gmail.com>
2024-01-11 11:44:02 -0800

    Fix aspect ratio

--- src/lib/components/subreddit/embed/RedditImage.svelte
+++ src/lib/components/subreddit/embed/RedditImage.svelte
@@ -4,23 +4,21 @@
 
 	export let submission: SubmissionData;
 
-	$: defaultImage = submission.preview.images.at(0)?.source;
-	$: defaultImageUrl = submission.preview.images.at(0)?.source.url ?? '';
+	$: defaultImage = submission.preview.images.at(0)?.resolutions.at(0);
+	$: defaultImageUrl = defaultImage?.url ?? '';
 	$: srcsetAndSizes = getSrcsetAndSizes(submission);
-	$: aspectRatio = (defaultImage?.height ?? 0) / (defaultImage?.width ?? 1);
+	$: aspectRatio = (defaultImage?.width ?? 0) / (defaultImage?.height ?? 1);
 
 	$: spoiler = submission.spoiler;
 	$: nsfw = submission.over_18;
 </script>
 
-<div class="relative max-h-[512px] overflow-hidden rounded-2xl border border-[#303030]">
-	<img
-		class="absolute left-0 top-1/2 h-full w-full -translate-y-1/2 scale-[1.2] object-cover opacity-30 blur-xl"
-		src={defaultImageUrl}
-		srcset={srcsetAndSizes?.srcset}
-		sizes={srcsetAndSizes?.sizes}
-		alt="{submission.subreddit_name_prefixed} - {submission.title}"
-	/>
+{defaultImage?.width}
+<div
+	class="background-stuff relative z-0 max-h-[512px] overflow-hidden rounded-2xl border border-[#303030]"
+	style="background-image: linear-gradient(rgba(27, 27, 27, 0.8), rgba(27, 27, 27, 0.8)), url({defaultImageUrl});"
+>
+	<div class="background-filter"></div>
 	<img
 		style:aspect-ratio={aspectRatio}
 		class="h-full max-h-[512px] w-full object-contain"
@@ -30,3 +28,23 @@
 		alt="{submission.subreddit_name_prefixed} - {submission.title}"
 	/>
 </div>
+
+<style>
+	.background-filter {
+		backdrop-filter: blur(16px);
+		content: '';
+		display: block;
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		z-index: -1;
+	}
+
+	.background-stuff {
+		position: relative;
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: cover;
+	}
+</style>
