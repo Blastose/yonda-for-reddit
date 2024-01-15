5a3b15bdc1fbc93865a2454624e2a04e2ed717f0
Blastose <blastoisejay@gmail.com>
2024-01-15 15:56:39 -0800

    Refactor out

---
+++ src/lib/components/subreddit/SubmissionCommentsContainer.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import CommentSort from '../comment/CommentSort.svelte';
+	import Comment from '../comment/Comment.svelte';
+	import type { SubmissionFull } from '$lib/reddit/reddit';
+	import type { Sort } from 'jsrwrap/types';
+	import { setSubmissionStore } from '$lib/stores/submissionStore';
+	import { transformUrlForIDBKey } from '$lib/url/url';
+	import { page } from '$app/stores';
+	import { db } from '$lib/idb/idb';
+
+	export let submission: SubmissionFull;
+	export let sort: Sort | undefined;
+
+	const persistSubmission = () => {
+		setSubmissionStore(transformUrlForIDBKey($page.url), submission);
+		db.put('subredditv2', submission, transformUrlForIDBKey($page.url));
+	};
+</script>
+
+<div class="flex flex-col gap-8">
+	<div class="flex flex-col gap-2">
+		<CommentSort {submission} />
+
+		<input
+			class="w-full rounded-3xl bg-[var(--search-input-bg)] px-4 py-2"
+			type="text"
+			placeholder="Add a comment"
+		/>
+	</div>
+
+	{#each submission.comments as comment, index}
+		<Comment
+			{comment}
+			pageSort={sort}
+			suggestedSort={submission.suggested_sort}
+			submissionId={submission.id}
+			updateReplies={(moreId, children) => {
+				const moreIndex = submission.comments.findIndex((v) => v.id === moreId);
+				console.log(moreIndex);
+				submission.comments.splice(moreIndex, 1);
+				submission.comments = submission.comments.concat(children);
+			}}
+			{persistSubmission}
+		/>
+		{#if index < submission.comments.length - 1}
+			<div class="flex justify-center">
+				<hr class="w-2/3 border-[#3a3a3a]" />
+			</div>
+		{/if}
+	{/each}
+</div>
--- src/routes/r/[subreddit]/comments/[submissionId]/(submission)/[submissionTitle]/+page.svelte
+++ src/routes/r/[subreddit]/comments/[submissionId]/(submission)/[submissionTitle]/+page.svelte
@@ -1,11 +1,6 @@
 <script lang="ts">
-	import Comment from '$lib/components/comment/Comment.svelte';
 	import CommentSkeleton from '$lib/components/comment/CommentSkeleton.svelte';
-	import CommentSort from '$lib/components/comment/CommentSort.svelte';
-	import { db } from '$lib/idb/idb.js';
-	import { setSubmissionStore } from '$lib/stores/submissionStore.js';
-	import { transformUrlForIDBKey } from '$lib/url/url.js';
-	import { page } from '$app/stores';
+	import SubmissionCommentsContainer from '$lib/components/subreddit/SubmissionCommentsContainer.svelte';
 
 	export let data;
 </script>
@@ -18,41 +13,6 @@
 			{/each}
 		</div>
 	{:then submission}
-		{@const persistSubmission = () => {
-			setSubmissionStore(transformUrlForIDBKey($page.url), submission);
-			db.put('subredditv2', submission, transformUrlForIDBKey($page.url));
-		}}
-		<div class="flex flex-col gap-8">
-			<div class="flex flex-col gap-2">
-				<CommentSort {submission} />
-
-				<input
-					class="w-full rounded-3xl bg-[var(--search-input-bg)] px-4 py-2"
-					type="text"
-					placeholder="Add a comment"
-				/>
-			</div>
-
-			{#each submission.comments as comment, index}
-				<Comment
-					{comment}
-					pageSort={data.sort}
-					suggestedSort={submission.suggested_sort}
-					submissionId={submission.id}
-					updateReplies={(moreId, children) => {
-						const moreIndex = submission.comments.findIndex((v) => v.id === moreId);
-						console.log(moreIndex);
-						submission.comments.splice(moreIndex, 1);
-						submission.comments = submission.comments.concat(children);
-					}}
-					{persistSubmission}
-				/>
-				{#if index < submission.comments.length - 1}
-					<div class="flex justify-center">
-						<hr class="w-2/3 border-[#3a3a3a]" />
-					</div>
-				{/if}
-			{/each}
-		</div>
+		<SubmissionCommentsContainer {submission} sort={data.sort} />
 	{/await}
 </div>
