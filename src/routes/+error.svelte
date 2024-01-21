50f0c88b3abb6b51b9105ad421bc2d56195fb9ae
Blastose <blastoisejay@gmail.com>
2024-01-20 19:30:37 -0800

    Add error page for firefox

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+{#if $page.url.searchParams.get('code')}
+	<div class="container-doku">
+		<p>
+			Firefox (or your current browser) cannot receive an access token from Reddit after redirecting
+			back.
+		</p>
+		<p>This can be fixed by refreshing the page.</p>
+		<p>Please refresh the page manually to log in.</p>
+	</div>
+{/if}
